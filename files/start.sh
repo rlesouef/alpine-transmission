f6f85c9f81aa1c6ccb514f858c8690b1123d8dd8
rlesouef <rlesouef@gmail.com>
2015-09-28 16:52:45 +0400

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -3,13 +3,13 @@ set -e
 SETTINGS=/etc/transmission-daemon/settings.json
 
 if [[ ! -f ${SETTINGS}.bak ]]; then
-    if [ -z $rpcpassword ]; then
-        echo Please provide a password for the 'transmission' user via the rpcpassword enviroment variable.
+    if [ -z $PASSWORD ]; then
+        echo Please provide a password for the 'transmission' user via the PASSWORD enviroment variable.
         exit 1
     fi
-    sed -i.bak -e "s/#rpc-password#/$rpcpassword/" $SETTINGS
-    sed -i.bak -e "s/#rpc-username#/$rpcusername/" $SETTINGS
+    sed -i.bak -e "s/#rpc-password#/$PASSWORD/" $SETTINGS
+    sed -i.bak -e "s/#rpc-username#/$USERNAME/" $SETTINGS
 fi
 
-unset rpcpassword rpcusername
+unset PASSWORD USERNAME
 exec /usr/bin/supervisord
